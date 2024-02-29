179e2cdfa213472b6c9f1fc327266e4dd529c24a
CameronChurchwell <steamtrains@icloud.com>
2024-02-29 11:43:25 -0600

    Initial Commit

---
+++ PPGMirror/PPGMirrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PPGMirrorApp.swift
+//  PPGMirror
+//
+//  Created by Steam Train on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct PPGMirrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
